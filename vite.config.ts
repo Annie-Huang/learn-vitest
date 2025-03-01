ba7bb41c029d8e0e2a36423f3c181c9e7d389fcb
Annie Huang <annie.hn@gmail.com>
2025-03-01 22:38:28 +1100

    Add configure in vite.config.ts and setup.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+// https://vitest.dev/guide/#configuring-vitest
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    // https://www.npmjs.com/package/happy-dom
+    environment: 'happy-dom',
+    setupFiles: ['./src/tests/setup.ts'],
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+  },
+});
