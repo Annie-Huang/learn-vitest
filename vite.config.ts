89367257aeebe57f87cc76a38857e1e6e9a42df3
Annie Huang <annie.hn@gmail.com>
2025-03-02 16:33:39 +1100

    Switch to use the jsdom also not helping to resolve the "Unresolved function or method toBeInTheDocument()" error

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   plugins: [react()],
   test: {
     // https://www.npmjs.com/package/happy-dom
-    environment: 'happy-dom',
+    // environment: 'happy-dom',
+    environment: 'jsdom',
     setupFiles: ['./src/tests/setup.ts'],
     include: ['src/**/*.{test,spec}.{ts,tsx}'],
     // above will not run sum.test.ts
